bcb291269744d1f981ddd987af3ca2dbe4fb90ec
AMendoza <allanmaleman@gmail.com>
2025-10-25 20:36:15 -0700

    FEAT: se trabaja en formulario de contacto

--- src/components/API/email.ts
+++ src/components/API/email.ts
@@ -1,5 +1,20 @@
 import type {APIRoute} from "astro";
+import {Resend} from "resend";
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
 export const GET: APIRoute = async () => {
-    return new Response(JSON.stringify({success: true}))
+    // send an email
+    const {data, error} = await resend.emails.send({
+        from: 'Allan <allanmaleman@gmail.com>',
+        to: ['delivered@resend.dev'],
+        subject: 'Hello world',
+        html: "<strong>It works!</strong>"
+    });
+
+    if (error) {
+        return new Response(JSON.stringify(error));
+    }
+
+    return new Response(JSON.stringify(data))
 };
